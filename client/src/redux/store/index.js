3a63f2624c432c9fa9c41bde3eb3cab43acd8140
Santiago <srueda9331@gmail.com>
2022-08-18 12:07:32 -0500

    add features store and reducer complete config

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,10 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
-import { compeseWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducer/index';
 import thunk from 'redux-thunk'
 
 
-
-const store = createStore(rootReducer, compeseWithDevTools(applyMiddleware(thunk)))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store;
