26ed13e209293d909e9eeb48fcdc863923294999
Andres Guerrero <94151800+andresguerrero270895@users.noreply.github.com>
2022-08-18 14:02:42 -0500

    (feat1: api/src/models: adding models and his properties -feat2:api/src/db: adding relationships between models

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,12 +1,55 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, UUIDV4 } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
+  sequelize.define('product', {
+
+    id :{ 
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    }, 
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    sku:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price :{ 
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weight :{ 
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    height :{ 
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    description :{ 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image :{ 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // brand :{ 
+    //   type: DataTypes.ENUM(['Samsung', 'Apple', 'Huawei', 'Xiaomi', 'Motorola']),
+    //   allowNull: false,
+    // },
+    quantity:{ 
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    stock :{ 
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    }
   });
 };
