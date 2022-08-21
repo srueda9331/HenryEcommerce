7b2048e0f717703db4fa1026206df76d7ce66313
Nicolas Ala <nicoalmeda@gmail.com>
2022-08-20 21:00:13 -0300

    Estilos al Detail y correciones

--- client/src/components/CardDetail/CardDetail.jsx
+++ client/src/components/CardDetail/CardDetail.jsx
@@ -3,8 +3,10 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 // import ItemCount from "../ItemCount/ItemCount";
 import "./CardDetail.css";
-import { cleanDetail, getPhoneDetail } from "../../redux/actions/actionCreators";
-
+import {
+  cleanDetail,
+  getPhoneDetail,
+} from "../../redux/actions/actionCreators";
 
 function CardDetail(props) {
   // const { id } = useParams();
@@ -31,14 +33,13 @@ function CardDetail(props) {
 
   useEffect(() => {
     dispatch(getPhoneDetail(id));
-    return () => dispatch(cleanDetail())
-  }, [id]);
+    return () => dispatch(cleanDetail());
+  }, [id, dispatch]);
 
   const phone = useSelector((state) => state.detail);
   console.log(phone[0]);
 
   return (
-    
     <div>
       {/* <div className="card_left">
         <img src={phone[0].image} alt="" />
@@ -66,33 +67,54 @@ function CardDetail(props) {
           )}
         </div>
       </div> */}
-      {
-        phone[0]? 
-          (
-            <div>
-              <h1>Name: {phone[0].name}</h1> 
-              <img src={phone[0].image}/>
-              <h3>Price: {phone[0].price}</h3>
-              {/* <h2> Brand: {phone[0].brand}</h2> */}
-              <p><span style={{fontWeight: 'bold'}}>Description:</span> {phone[0].description}</p>
-              <h4>Height: {phone[0].height}</h4>
-              <h4>Weight: {phone[0].weight}</h4>
-              <h4>Rating: {phone[0].rating}</h4>
-              {
-                phone[0].quantity > 0? <h3>Stock: True</h3> : <h3>Stock: false</h3>
-              }
-              <h4>Review : {phone[0].review}</h4>
+      {phone[0] ? (
+        // <div>
+        //   <h1>Name: {phone[0].name}</h1>
+        //   <img src={phone[0].image} alt="" />
+        //   <h3>Price: {phone[0].price}</h3>
+        //   {/* <h2> Brand: {phone[0].brand}</h2> */}
+        //   <p>
+        //     <span style={{ fontWeight: "bold" }}>Description:</span>{" "}
+        //     {phone[0].description}
+        //   </p>
+        //   <h4>Height: {phone[0].height}</h4>
+        //   <h4>Weight: {phone[0].weight}</h4>
+        //   <h4>Rating: {phone[0].rating}</h4>
+        //   {phone[0].quantity > 0 ? <h3>Stock: True</h3> : <h3>Stock: false</h3>}
+        //   <h4>Review : {phone[0].review}</h4>
+        // </div>
 
+        <div className="container">
+          <div className="card_left">
+            <img src={phone[0].image} alt="" />
+          </div>
+          <div className="card_right">
+            <h3 className="title-product">{phone[0].name}</h3>
+            <p className="product-description">{phone[0].description}</p>
 
+            <div className="detail-container">
+              <div className="cont">
+                <strong>Weight:</strong>
+                <h4 className="detail-description">{phone[0].weight}</h4>
+              </div>
+              <div className="cont">
+                <strong>Raiting:</strong>
+                <h4 className="detail-description">{phone[0].rating}</h4>
+              </div>
+              <div className="cont">
+                <strong>Review:</strong>
+                <h4 className="detail-description">{phone[0].review}</h4>
+              </div>
             </div>
-          
-          )
-          : 
-          (
-            <h1>{`LO SENTIMOS, NO EXISTE NINGÚN CELULAR CON ID "${id}"`}</h1>
-          )
-      }
-      {/* <div>{name}</div> */}
+
+            <div className="card_footer">
+              <span className="price">{phone[0].price} $</span>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <h1>{`LO SENTIMOS, NO EXISTE NINGÚN CELULAR CON ID "${id}"`}</h1>
+      )}
     </div>
   );
 }
