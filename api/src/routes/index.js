0719a3974c1d5b5beafbf9153c85c8c31bfbd66c
NotSpawn <facundojuarez46@gmail.com>
2022-08-20 23:06:09 -0300

    Login added
    Login funcionality added (bcrypt , jwt)

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,10 +4,11 @@ const { Router } = require("express");
 
 const getBrands = require("../routes/getBrands");
 const getInfo = require("../routes/getPhone");
+const getCusto = require("../routes/getCustomers");
 
 const router = Router();
 
 router.use("/phones", getInfo);
 router.use("/brands", getBrands);
-
+router.use("/users", getCusto);
 module.exports = router;
