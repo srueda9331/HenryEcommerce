98d95f9705174bb6f2648bccfe28bbe492c3ce13
Santiago <srueda9331@gmail.com>
2022-08-22 12:49:29 -0500

    paginado

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -24,7 +24,7 @@ function Card({ id, name, image, brand, price }) {
                   <div className="img-info">
                     <div className="info-inner">
                       <span className="p-name">
-                        {name.length > 10? name[0].toUpperCase() + name.slice(2,30) : name }
+                      {name.length > 10? name[0].toUpperCase() + name.slice(2,30) : name }
                       </span>
                     </div>
                     <div className="a-size">
