c5d512defe11627a07f4a99314a97188a976b873
NotSpawn <facundojuarez46@gmail.com>
2022-09-06 23:28:08 -0300

    order products map buy history added
    order products map buy history added

--- client/src/Components/User/UserBuyHistory/OrderCard/OrderCard.jsx
+++ client/src/Components/User/UserBuyHistory/OrderCard/OrderCard.jsx
@@ -1,32 +1,15 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
+import ItemCard from '../ItemCard/ItemCard';
 
-function OrderCard({
-  id,
-  status,
-  date,
-  title,
-  price,
-  quantity,
-  image,
-  productid,
-}) {
+function OrderCard({ id, status, date, order }) {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={image} />
       <Card.Body>
         <Card.Title>{id}</Card.Title>
-        <Card.Title>{title}</Card.Title>
+        <Card.Title>{date}</Card.Title>
         <Card.Title>{status}</Card.Title>
-        <Card.Text>
-          {date} , {price}0,{quantity},{image},{productid}
-        </Card.Text>
-        .
-        <Link to={`/detalle/${productid}`}>
-          <Button variant="primary">Ver detalle</Button>
-        </Link>
+        <ItemCard order={order} />
       </Card.Body>
     </Card>
   );
